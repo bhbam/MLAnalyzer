a2aeebb7201ca28340d894025fcc125a6767be10
bhbam <bbbam@crimson.ua.edu>
2023-11-26 02:32:44 -0600

    input file for test added Tau AOD

--- runRHAnalyzer_data.py
+++ runRHAnalyzer_data.py
@@ -1,10 +1,10 @@
 import os
 
 cfg='RecHitAnalyzer/python/ConfFile_data_cfg.py'
-inputFiles_='file:../Run2018A_Tau_AOD_12Nov2019_UL2018_1E64EE69-D0C6-3E47-9724-FDF606418876.root'
+inputFiles_='file:data_Run2018B_Tau_AOD_15Feb2022_UL2018_v1_2820000_0C6EC09F_736E_B640_AD5A_E99536E72323.root'
 #inputFiles_='file:../Run2018A_Tau_AOD_12Nov2019_UL2018_D0938794-D6B0-594C-BB05-5CEE65208347.root'
 
-maxEvents_=-1
+maxEvents_=10
 skipEvents_=0#
 
 
